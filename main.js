61f433c5538c90d9f318275fb5dd594b439f45a7
juandacampoc <juandavidcampo@outlook.com>
2022-10-07 11:14:01 -0500

    inicios de funcioanlidades

---
+++ main.js
@@ -0,0 +1,12 @@
+let minusBtn = document.querySelector('.input__minus');
+let plusBtn = document.querySelector('.input__plus');
+let userInput = document.querySelector('.input__number');
+
+let userInputNumber = 0;
+
+plusBtn.addEventListener('click',()=>{
+    console.log("se presiono mas")
+})
+minusBtn.addEventListener('click',()=>{
+    console.log("se presiono menos")
+})
